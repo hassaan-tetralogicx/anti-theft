10073647bb890d36bb9cb82e6b8721220d19a060
hassaan-tetralogicx <hassan.majeed@tetralogicx.com>
2021-01-22 14:10:38 +0500

    render

---
+++ resources/views/response.blade.php
@@ -0,0 +1,23 @@
+
+    @if ($check_setting != null)
+        @if($check_setting->text_protection == 1)
+
+        document.addEventListener('copy paste cut', event => event.preventDefault());
+            document.onkeydown = function (e) {
+            return false;
+            }; document.addEventListener('contextmenu', event => event.preventDefault());
+
+        @endif
+        @if($check_setting->image_protection == 1)
+
+            document.ondragstart = function () {
+            return false;
+            };
+            document.onkeydown = function (e) {
+            return false;
+            };
+            document.addEventListener('contextmenu', event => event.preventDefault());
+
+        @endif
+    @endif
+
